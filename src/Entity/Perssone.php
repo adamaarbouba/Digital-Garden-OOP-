cdecc7aff1a2f5522f9f1cf866e52cb161ec8c32
mourad kadiri <mouradkadi2001@gmail.com>
2025-12-31 16:53:57 +0100

    modification sur la partie des creation des classes

--- src/Entity/Perssone.php
+++ src/Entity/Perssone.php
@@ -1,32 +1,31 @@
 <?php
 
-class Perssone {
-    
-    protected $id;
-    protected $username;
-    protected $email;
-    protected $password;
-    protected $role;
+class Perssone
+{
 
-    public function __construct($username , $email , $password ,$id = null){
-        $this->username=$username;
-        $this->email=$email;
-        $this->password=$password;
-    }
+  protected $id;
+  protected $username;
+  protected $email;
+  protected $password;
+  protected $role;
 
-    public function __set($name, $value){
-        $this->$name=$value;
-    }
+  public function __construct($username, $email, $password, $id = null)
+  {
+    $this->username = $username;
+    $this->email = $email;
+    $this->password = $password;
+  }
 
-    public function __get($name){
-       return $this->$name;
-    }
+  public function getRole()
+  {
+    return $this->role;
 
-    public function setid($id){
-      if (!is_numeric($id) || (int) $id <=0) {
-        die ("wrond person iD");
-      }
-      $this->$id;
-    }
+  }
+
+  public function __get($name)
+  {
+    return $this->$name;
+  }
+
 
 }
